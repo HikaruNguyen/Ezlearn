677221d71dc86f141fdf2f89c66c86c0d9d0bbc7
HikaruNguyen <Manh.it.it.it@gmail.com>
2017-09-14 10:33:30 +0700

    Add list home

--- app/src/main/java/com/vn/ezlearn/fragment/HomeFragment.java
+++ app/src/main/java/com/vn/ezlearn/fragment/HomeFragment.java
@@ -44,7 +44,7 @@ public class HomeFragment extends Fragment implements BaseSliderView.OnSliderCli
         DisplayMetrics displayMetrics = getActivity().getResources().getDisplayMetrics();
         int width = displayMetrics.widthPixels;
         float height = width* 485/1366;
-
+        homeBinding.rlSlide.setVisibility(View.VISIBLE);
         homeBinding.rlSlide.setLayoutParams(new RelativeLayout.LayoutParams(width, (int) height));
 
         homeBinding.slider.setPresetTransformer(SliderLayout.Transformer.Accordion);
