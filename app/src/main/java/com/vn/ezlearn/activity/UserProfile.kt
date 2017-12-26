e1c6dacd1c2e6f4cf6d0dfeca2fc3c2a3ccfa8ad
oNguyenDucManh <Manh.it.it.it@gmail.com>
2017-12-26 09:11:20 +0700

    fix userprofile

--- app/src/main/java/com/vn/ezlearn/activity/UserProfile.kt
+++ app/src/main/java/com/vn/ezlearn/activity/UserProfile.kt
@@ -2,23 +2,29 @@ package com.vn.ezlearn.activity
 
 import android.databinding.DataBindingUtil
 import android.os.Bundle
-import android.support.v4.app.Fragment
 import com.vn.ezlearn.R
-import com.vn.ezlearn.adapter.ViewPagerAdapter
+import com.vn.ezlearn.adapter.UserPackageAdapter
+import com.vn.ezlearn.config.EzlearnService
 import com.vn.ezlearn.databinding.ActivityUserProfileBinding
-import com.vn.ezlearn.fragment.UserProfileFragment
 import com.vn.ezlearn.interfaces.UserInfoCallBack
+import com.vn.ezlearn.modelresult.UserInfoResult
 import com.vn.ezlearn.models.User
 import com.vn.ezlearn.viewmodel.UserMainViewModel
-import java.util.*
+import rx.Subscriber
+import rx.Subscription
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
 
 
 class UserProfile : BaseActivity(), UserInfoCallBack {
 
     private lateinit var userProfileBinding: ActivityUserProfileBinding
-    private var fragmentList: MutableList<Fragment>? = null
-    private var viewPagerAdapter: ViewPagerAdapter? = null
+    //    private var fragmentList: MutableList<Fragment>? = null
+//    private var viewPagerAdapter: ViewPagerAdapter? = null
     private lateinit var userMainViewModel: UserMainViewModel
+    private var apiService: EzlearnService? = null
+    private var mSubscription: Subscription? = null
+    private lateinit var mUserInfoResult: UserInfoResult
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,18 +35,50 @@ class UserProfile : BaseActivity(), UserInfoCallBack {
         setSupportActionBar(userProfileBinding.toolbar)
         setBackButtonToolbar()
         userProfileBinding.toolbar.title = ""
+        apiService = MyApplication.with(this).getEzlearnService()
         bindData()
 
     }
 
     private fun bindData() {
-        fragmentList = ArrayList()
-        val userProfileFragment = UserProfileFragment()
-        userProfileFragment.setUserInfoCallBack(this@UserProfile)
-        fragmentList!!.add(userProfileFragment)
-        viewPagerAdapter = ViewPagerAdapter(supportFragmentManager, fragmentList!!)
-        userProfileBinding.viewPager.adapter = viewPagerAdapter
+//        fragmentList = ArrayList()
+//        val userProfileFragment = UserProfileFragment()
+//        userProfileFragment.setUserInfoCallBack(this@UserProfile)
+//        fragmentList!!.add(userProfileFragment)
+//        viewPagerAdapter = ViewPagerAdapter(supportFragmentManager, fragmentList!!)
+//        userProfileBinding.viewPager.adapter = viewPagerAdapter
+        if (mSubscription != null && !mSubscription!!.isUnsubscribed)
+            mSubscription!!.unsubscribe()
+        mSubscription = apiService!!.getUserInfo()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(object : Subscriber<UserInfoResult>() {
+                    override fun onCompleted() {
+                        if (mUserInfoResult.success!!) {
+                            mUserInfoResult.data?.let {
+                                if (it.user_packages != null) {
+                                    val userPackageAdapter = UserPackageAdapter(this@UserProfile, ArrayList())
+                                    userProfileBinding.rvUserPackage.adapter = userPackageAdapter
+                                    userPackageAdapter.addAll(it.user_packages!!)
+                                }
+                                if (it.user != null) {
+                                    userMainViewModel.setUserInfo(it.user!!)
+                                }
+                            }
 
+                        }
+                    }
+
+                    override fun onError(e: Throwable) {
+
+                    }
+
+                    override fun onNext(userInfoResult: UserInfoResult?) {
+                        if (userInfoResult != null) {
+                            mUserInfoResult = userInfoResult
+                        }
+                    }
+                })
     }
 
     override fun onLoadUserInfoSuccess(user: User?) {
--- app/src/main/java/com/vn/ezlearn/viewmodel/UserMainViewModel.kt
+++ app/src/main/java/com/vn/ezlearn/viewmodel/UserMainViewModel.kt
@@ -18,30 +18,68 @@ class UserMainViewModel(private var context: Context) {
     var tvFullName: ObservableField<String> = ObservableField("")
     var tvLevelName: ObservableField<String> = ObservableField("")
     var tvWallet: ObservableField<String> = ObservableField("")
+    var tvWalletInfo: ObservableField<String> = ObservableField("")
     var tvMarkAccumulation: ObservableField<String> = ObservableField("")
+    var tvMarkAccumulationInfo: ObservableField<String> = ObservableField("")
     var tvFreeTimesWorking: ObservableField<String> = ObservableField("")
+    var tvFreeTimesWorkingInfo: ObservableField<String> = ObservableField("")
+
+    var tvUsername: ObservableField<String> = ObservableField("")
+    var tvFirstName: ObservableField<String> = ObservableField("")
+    var tvLastName: ObservableField<String> = ObservableField("")
+    var tvLevelStudyName: ObservableField<String> = ObservableField("")
 
     fun setUserInfo(user: User) {
         if (!TextUtils.isEmpty(user.full_name)) {
             tvFullName.set(user.full_name)
+        } else {
+            tvFullName.set(context.getString(R.string.null_data))
         }
+
         if (!TextUtils.isEmpty(user.level_name)) {
             tvLevelName.set(context.getString(R.string.level, user.level_name))
+        } else {
+            tvLevelName.set(context.getString(R.string.level, context.getString(R.string.null_data)))
         }
         if (user.mark_accumulation != null) {
             tvMarkAccumulation.set(user.mark_accumulation.toString())
+            tvMarkAccumulationInfo.set(context.getString(R.string.achievements_point,
+                    user.mark_accumulation))
         } else {
-            tvMarkAccumulation.set("0")
+            tvMarkAccumulation.set(context.getString(R.string.null_data))
+            tvMarkAccumulationInfo.set(context.getString(R.string.null_data))
         }
         if (user.wallet != null) {
             tvWallet.set(AppUtils.formatMoney(user.wallet!!))
+            tvWalletInfo.set(context.getString(R.string.balance_vnd,
+                    AppUtils.formatMoney(user.wallet!!)))
         } else {
-            tvWallet.set("0")
+            tvWallet.set(context.getString(R.string.null_data))
+            tvWalletInfo.set(context.getString(R.string.null_data))
         }
         if (user.free_times_working != null) {
             tvFreeTimesWorking.set(user.free_times_working!!.toString() + "")
+            tvFreeTimesWorkingInfo.set(user.free_times_working!!.toString() + "")
         } else {
-            tvFreeTimesWorking.set("0")
+            tvFreeTimesWorking.set(context.getString(R.string.null_data))
+            tvFreeTimesWorkingInfo.set(context.getString(R.string.null_data))
+        }
+
+        /*=================*/
+        if (!TextUtils.isEmpty(user.username)) {
+            tvUsername.set(user.username)
+        } else {
+            tvUsername.set(context.getString(R.string.null_data))
+        }
+        if (!TextUtils.isEmpty(user.first_name)) {
+            tvFirstName.set(user.first_name)
+        } else {
+            tvFirstName.set(context.getString(R.string.null_data))
+        }
+        if (!TextUtils.isEmpty(user.last_name)) {
+            tvLastName.set(user.last_name)
+        } else {
+            tvLastName.set(context.getString(R.string.null_data))
         }
     }
 }
