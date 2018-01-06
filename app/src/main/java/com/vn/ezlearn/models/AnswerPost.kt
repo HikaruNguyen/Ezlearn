f1d778c581ec0b3ef49ae249a6035d66391b337b
Manhnd <Manh.it.it.it@gmail.com>
2018-01-06 20:17:22 +0700

    answer post

---
+++ app/src/main/java/com/vn/ezlearn/models/AnswerPost.kt
@@ -0,0 +1,8 @@
+package com.vn.ezlearn.models
+
+/**
+ * Created by admin on 5/1/2018.
+ */
+
+class AnswerPost(private val qId: String, private val answer: String, private val answer_true: Int?,
+                 private val approved: Int?, private val rId: String)
